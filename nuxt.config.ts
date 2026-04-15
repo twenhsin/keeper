49a77b2de77c6d656599406741921364d2e5461e
Lab_kelly <kelly@kylab.io>
2026-04-15 15:39:49 +0800

    手機輸入匡介面變大修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       meta: [
-        { name: 'viewport', content: 'width=device-width, initial-scale=1, viewport-fit=cover' }
+        { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' }
       ]
     }
   }
